dd263dd1c70486cbf0207d5cc91cf75efde0ca44
cs4151 <hannah.gabele@student.uibk.ac.at>
2025-03-20 18:11:43 +0100

    skribt oben eingebunden

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hallo!");
+var map = L.map('map').setView([-41.2875, 174.776111], 12);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([-41.2875, 174.776111]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
